a68043f10b81e847a273bbe1e9ccf069fcb27fb0
jewelcse <jewelcse045@gmail.com>
2023-04-06 10:19:36 +0600

    @component

--- src/main/java/com/anotationpractice/AnotationPracticeApplication.java
+++ src/main/java/com/anotationpractice/AnotationPracticeApplication.java
@@ -10,18 +10,14 @@ public class AnotationPracticeApplication {
 
     public static void main(String[] args) {
         var context = SpringApplication.run(AnotationPracticeApplication.class, args);
-
-
         // Using the direct class name, we can get the bean
         PizzaService pizzaService0 = context.getBean(PizzaService.class);
         System.out.println(pizzaService0.getVegPizza());
-
         // class name can also be treated as a bean name if and only if the first letter of
         // the class is small letter, and we will get
         // bean not found runtime exception will be occurred if the bean name as the same as class name
         PizzaService pizzaService1 = (PizzaService) context.getBean("pizzaService");
         System.out.println(pizzaService1.getVegPizza());
-
         // @component(value="mypizza") is used to get the bean
         // and need to typecast the class
         //PizzaService pizzaService2 = (PizzaService) context.getBean("mypizza");
