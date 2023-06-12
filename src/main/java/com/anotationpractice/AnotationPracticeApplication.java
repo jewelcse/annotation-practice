3034ca04c6d537723b55de5b9f35172f4b781b55
jewelcse <jewelcse045@gmail.com>
2023-06-12 11:03:23 +0600

    @autoried annotation updated

--- src/main/java/com/anotationpractice/AnotationPracticeApplication.java
+++ src/main/java/com/anotationpractice/AnotationPracticeApplication.java
@@ -10,12 +10,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class AnotationPracticeApplication {
 
     public static void main(String[] args) {
+
         var context = SpringApplication.run(AnotationPracticeApplication.class, args);
 
         PizzaController controller = context.getBean(PizzaController.class);
         System.out.println(controller.getPizza());
-
-
     }
 
 }
